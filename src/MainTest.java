a5e2e118fe8e05918e35772704c46bf2b7b5ad63
jackythesmurf <Jackythesmurf@gmail.com>
2023-02-25 21:32:44 +1100

    testing for testonStart()

--- src/MainTest.java
+++ src/MainTest.java
@@ -7,58 +7,69 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
 
-    public static void setUserInput(String userInput){
+    public static void setUserInput(String userInput) {
         InputStream in = new ByteArrayInputStream(userInput.getBytes());
         System.setIn(in);
     }
 
     @Test
-    void onStart() {
-
-    }
-
-
-    @Test
-    void selectDifficultyEasy() {
-        ArrayList<String> validEasyInputs = new ArrayList<>(){{
-            add("easy"); add("EASY"); add(" easy "); add(" Easy ");
+    void selectDifficultyEasy() throws Main.InvalidOption {
+        ArrayList<String> validEasyInputs = new ArrayList<>() {
+            {
+                add("easy");
+                add("EASY");
+                add(" easy ");
+                add(" Easy ");
             }
         };
-        for (String userInput: validEasyInputs){
+        for (String userInput : validEasyInputs) {
             setUserInput(userInput);
             Main.selectDifficulty();
             assertEquals(Main.getWidth(), 10);
             assertEquals(Main.getNumOfMines(), 20);
         }
     }
+
     @Test
-    void selectDifficultyHard() {
-        ArrayList<String> validEasyInputs = new ArrayList<>(){{
-            add("hard"); add("HARD"); add(" hard "); add(" Hard ");
+    void selectDifficultyHard() throws Main.InvalidOption {
+        ArrayList<String> validEasyInputs = new ArrayList<>() {
+            {
+                add("hard");
+                add("HARD");
+                add(" hard ");
+                add(" Hard ");
             }
         };
-        for (String userInput: validEasyInputs){
+        for (String userInput : validEasyInputs) {
             setUserInput(userInput);
             Main.selectDifficulty();
             assertEquals(Main.getWidth(), 15);
             assertEquals(Main.getNumOfMines(), 30);
         }
     }
+
     @Test
     void selectDifficultyInvalid() {
-        ArrayList<String> validEasyInputs = new ArrayList<>(){{
-            add("123"); add("easy."); add(""); add(" ");
+        ArrayList<String> validEasyInputs = new ArrayList<>() {
+            {
+                add("123");
+                add("easy.");
+                add("hard23");
+                add("easyhard");
             }
         };
-        for (String userInput: validEasyInputs){
+        for (String userInput : validEasyInputs) {
             setUserInput(userInput);
-            assertThrows(Exception.class, Main::selectDifficulty);
+            assertEquals("Not a valid String, please type 'easy' or 'hard'",
+                    assertThrows(Main.InvalidOption.class, Main::selectDifficulty).getMessage());
         }
-
     }
 
     @Test
     void testOnStart() {
+        Main.onStart();
+        assertEquals(15, Grid.getWidth());
+        assertEquals(30, Grid.getNumOfMines());
     }
 
     @Test
